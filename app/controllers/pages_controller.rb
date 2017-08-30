a949fa0df68cbc99e265fd5d16ad530d6c8dca43
TJ Checketts <tylerchecketts@gmail.com>
2017-08-29 18:59:48 -0600

    add pages to edit and delete and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,6 +25,25 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else  
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
   private
     def page_params
       params.require(:page).permit(:title, :author, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
-  root 'pages#index'
 
+  root 'static_pages#home'
   resources :pages
+
+  get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
